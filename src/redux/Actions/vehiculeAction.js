8ee1498ceaaba21fccd8e7e7efbb4a01a2cd1f9c
kayembajoel92@gmail.com <kayembajoel92@gmail.com>
2024-10-26 23:08:53 -0400

    ajout du vin non fonctionnel

--- src/redux/Actions/vehiculeAction.js
+++ src/redux/Actions/vehiculeAction.js
@@ -7,9 +7,11 @@ export const DELETE_VEHICULE = 'DELETE_VEHICULE';
 export const SEARCH_RECALLS = 'SEARCH_RECALLS';
 export const RESET_RECALLS = 'RESET_RECALLS';
 export const ERROR_SEARCH = 'ERROR_SEARCH';
+export const SEARCH_VEHICLE_BY_VIN = 'SEARCH_VEHICLE_BY_VIN';
 
 // API URL 
 const NHTSA_RECALLS_API_URL = 'https://api.nhtsa.gov/recalls/recallsByVehicle';
+const NHTSA_VIN_API_URL = 'https://api.nhtsa.gov/complaints/vin';
 
 // Action pour ajouter un véhicule
 export const addVehicule = (vehicule) => {
@@ -68,4 +70,30 @@ export const resetRecalls = () => {
   return {
     type: RESET_RECALLS,
   };
+};
+
+// Action pour rechercher un véhicule par VIN
+export const searchVehicleByVin = (vin) => async (dispatch) => {
+  try {
+    const response = await axios.get(`${NHTSA_VIN_API_URL}?vin=${vin}`);
+    const vehicleData = response.data.Results[0];
+
+    // Dispatch des résultats du VIN vers le reducer
+    dispatch({
+      type: SEARCH_VEHICLE_BY_VIN,
+      payload: vehicleData,
+    });
+  } catch (error) {
+    const errorMessage = error.response && error.response.data ? 
+      error.response.data.message || error.response.data :
+      error.message;
+
+    // Dispatch de l'erreur en cas de problème
+    dispatch({
+      type: ERROR_SEARCH,
+      payload: errorMessage,
+    });
+
+    console.error("Erreur lors de la recherche du VIN :", errorMessage);
+  }
 };
